3b8c6ff916b48a88930ce31206ff0dc299819aa6
Bessornia Danilo José <Danilo.Bessornia@gft.com>
2019-11-13 17:16:45 +0100

    Changes to be committed: 	modified:   branches_older_than.sh

--- branches_older_than.sh
+++ branches_older_than.sh
@@ -1,15 +1,12 @@
-# Copy of https://gist.github.com/antonio/4586456
-# With a modification to collect all the branch names so we can make one git request
 # Set DRY_RUN=1 to get an echo of the command
-
 # Format that works with `git log --since`, e.g. 2018-01-01
 date=$1
 
 branches=
 
-DRY_RUN=1
+DRY_RUN=0
 
-for branch in $(git branch -a | sed 's/^\s*//' | sed 's/^remotes\///' | grep -v 'master$'); do
+for branch in $(git branch -a | sed 's/^\s*//' | sed 's/^remotes\///' | grep -v 'master$' | grep -v 'feature*'| grep -v 'develop'); do
   if [[ "$(git log $branch --since $date | wc -l)" -eq 0 ]]; then
     if [[ "$branch" =~ "origin/" ]]; then
       if [[ -z $branches ]]; then
@@ -28,7 +25,7 @@ if [[ ! -z $branches ]]; then
   else
     #git branch -D $branches
     #git push --delete origin $branches
-    echo "Passou aqui deu merda!!!!!!!!!!!!!!!!!"
+    echo "If Pass here DRY RUN failed and you are be fired!!!!"
     # clean up locally
     #git remote prune origin
   fi
